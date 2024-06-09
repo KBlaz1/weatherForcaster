12d25e558901964b0963a0d5bfa644d791da318e
Blaz <kapun.cb@gmail.com>
2024-06-09 22:42:10 +0200

    weather service tests

--- src/app/components/weather/weather.component.spec.ts
+++ src/app/components/weather/weather.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { WeatherComponent } from './weather.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('WeatherComponent', () => {
   let component: WeatherComponent;
@@ -10,7 +11,7 @@ describe('WeatherComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ WeatherComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), HttpClientTestingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(WeatherComponent);
